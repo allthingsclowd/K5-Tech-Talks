f93490a9d25db263676b6187fa67bb35ce2501ff
Graham <allthingsclowd@users.noreply.github.com>
2017-01-11 09:40:43 +0000

    Added missing user details file to store passwords

---
+++ K5-API-Examples/k5contractsettingsV8.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+
+adminUser = 'username'
+adminPassword = 'password'
+contract = 'contract name'
+contractid = 'contract id'
+defaultid = 'default project id (the project that ends in -prj'
+### The following parameters will change depending on the apps you're running
+project = 'project that you wish to work on'
+region = 'uk-1'
+az1 = 'uk-1a'
+az2 = 'uk-1b'
+extaz1 = 'df8d3f21-75f2-412a-8fd9-29de9b4a4fa8'
+extaz2 = 'd730db50-0e0c-4790-9972-1f6e2b8c4915'
+demoProject = 'Project_A'
+demoProjectid = '7015d1478a4c4bd7b970215d7b0260dd'
+demoProject2 = 'Project_B'
+demoProjectid2 = 'd3ad6ee6570e41daa0c62663c77b8747'
+
+
